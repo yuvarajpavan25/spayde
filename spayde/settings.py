61409a96d8f657a6ea8657e795d16d827948eca5
yuvarajpavan25 <yuvarajpavan25@gamil.com>
2025-02-25 15:21:57 +0530

    Added build script, requirements, and updated settings and URLs

--- spayde/settings.py
+++ spayde/settings.py
@@ -16,6 +16,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
 # Quick-start development settings - unsuitable for production
