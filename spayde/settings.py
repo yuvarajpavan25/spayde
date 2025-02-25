e7a50370c0513489e39d82aa2fa069e680e30eab
yuvarajpavan25 <yuvarajpavan25@gamil.com>
2025-02-25 16:01:55 +0530

    settings.py

--- spayde/settings.py
+++ spayde/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-c)kh^hy+qz0v=)+xo8gp=+p_a#2@t5k9fbiru03=620+pcx!@j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
